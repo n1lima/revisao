3b4ffef30c807e4022316e9bb27ef515d168dad9
brocolis é bom <134985029+n1lima@users.noreply.github.com>
2024-09-11 11:24:34 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-from quiz import Quiz
-
-
-
-def inserirAluno():
-    aluno = input('Escreva seu nome:')
-    disciplina = input('Qual disciplina: ')
-    acertos = int(input('Quantas questões você acertou?'))
-    erros = int(input('Quantas questões você errou?'))
-    return Quiz(aluno, disciplina, acertos, erros)
-
+from quiz import *
 
 if __name__ == '__main__':
-    while True:
-        print('Bem vindo, aluno! Entre com seu nome ')
-        aluno_quiz = inserirAluno()
+    q1 = Quiz(12, 8)
+    q2 = Quiz2A(10, 10)
+    q3 = Quiz3A(5, 15)
 
-        print(aluno_quiz.get_acertos())
-        print(aluno_quiz.get_erros())
-        print(aluno_quiz.calcularPontuacao())
+    print(q1)
+    print(q2)
+    print(q3)
+
+    print('testando')
+    print(q1.get_acertos())
+    print(q1.get_erros())
+    print(q1.calcular_pontos())
+
+    lista_quiz = [q1, q2, q3]
+    a1 = Aluno(1, 'Bruna', lista_quiz)
+    a2 = Aluno(2, 'Carla', lista_quiz)
+    print(a1)
+
+    d = Disciplina('POO', 'Ferauche', 2024, 4)
+    d.adicionarAluno(a1)
+    d.adicionarAluno(a2)
+    print(d)
+
+
+
+
 
--- quiz.py
+++ quiz.py
@@ -1,7 +1,7 @@
+from typing import List
+
 class Quiz:
-    def __init__(self, disciplina, aluno, acertos, erros):
-        self.disciplina = disciplina
-        self.aluno = aluno
+    def __init__(self, acertos, erros):
         self.__acertos = acertos
         self.__erros = erros
 
@@ -15,9 +15,8 @@ class Quiz:
         return self.__erros
 
     def __str__(self):
-        info = f'Disiciplina: {self.disciplina}\n Aluno: {self.aluno}'
-        info += f'Acertos: {self.__acertos}\n Erros: {self.__erros}'
-        info += f'Total: {self.calcularPontuacao()}'
+        info = f'Acertos: {self.__acertos}\n Erros: {self.__erros}'
+        info += f' Total: {self.calcularPontuacao()}'
         return info
 
 class Quiz2A(Quiz):
@@ -27,3 +26,41 @@ class Quiz2A(Quiz):
 class Quiz3A(Quiz):
     def calcular_pontos(self):
         return self.get_acertos() - 2 *(self.get_erros())
+
+class Aluno:
+    def __init__(self, matricula: int, nome: str, quiz: List[Quiz]): #uma composição
+        self.__matricula = matricula
+        self.__nome = nome
+        self.__quiz = quiz
+
+    def __str__(self):
+        info = f'matricula: {self.__matricula}\n nome:{self.__nome}\n'
+        sum = 0
+        for q in self.__quiz:
+            sum += q.calcular_pontos()
+        info += f'Total: {sum}\n'
+        return info
+
+    def get_matricula(self):
+        return self.__matricula #para conseguirmos pegar a matricula que é um dado sensivel
+
+class Disciplina:
+    __alunos : List[Aluno] = []
+
+    def __init__(self, nome, professor, ano, semestre):
+        self.__nome = nome
+        self.__professor = professor
+        self.__ano = ano
+        self.__semestre = semestre
+
+    def adicionarAluno(self, a: Aluno):
+        for aluno in self.__alunos:
+            if aluno.get_matricula() == a.get_matricula():
+                raise Exception('ALuno já existe!')
+            self.__alunos.append(a)
+
+    def __str__(self):
+        info = f'Disciplina: {self.__nome}\n professor:{self.__professor}\n Ano: {self.__ano}\n Semestre: {self.__semestre}\n'
+        for aluno in self.__alunos:
+            info += f'{aluno}'
+        return info
